7901ed40325a4762e1665637684fd9a6658aec3f
Gabrielle Oliveira de Paula <gabrielleoliveirah14@gmail.com>
2025-11-22 19:41:17 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+MENUS = {
+    "principal": """
+1 - Relatórios
+2 - Inserir
+3 - Atualizar
+4 - Excluir
+5 - Sair
+""",
+    "entidades": """
+1 - Usuário
+2 - Tarefa
+0 - Voltar
+""",
+    "relatorios": """
+1 - Usuários
+2 - Tarefas
+3 - Tarefas por Usuário (GROUP BY)
+4 - Tarefas com JOIN
+0 - Voltar
+"""
+}
