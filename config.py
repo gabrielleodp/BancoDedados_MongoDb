c1ee18eee41909f966ed6faa4ee9f5e971fb1414
Gabrielle Oliveira de Paula <gabrielleoliveirah14@gmail.com>
2025-11-22 20:13:33 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 MENUS = {
     "principal": """
-1 - Relatórios
-2 - Inserir
-3 - Atualizar
-4 - Excluir
-5 - Sair
+1 - 🧾 Ver Relatórios
+2 - ✏️  Inserir dados
+3 - 🔄 Atualizar dados
+4 - 🗑️  Excluir itens
+5 - ❌ Sair 
 """,
     "entidades": """
-1 - Usuário
-2 - Tarefa
-0 - Voltar
+1 - 👤 Usuário
+2 - 📝 Tarefas
+0 - ↩️  Voltar
 """,
     "relatorios": """
-1 - Usuários
-2 - Tarefas
-3 - Tarefas por Usuário (GROUP BY)
-4 - Tarefas com JOIN
-0 - Voltar
+1 - 👥 Usuários cadastrados
+2 - 📝 Tarefas cadastradas
+3 - 📊 Quantidade de tarefas por Usuário
+4 - 🧾 Detalhamento de tarefa por usuário
+0 - ↩️  Voltar
 """
 }
